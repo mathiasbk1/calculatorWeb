dd4119f010414fd4d35409ffe31b857c3bc03d1a
Mathias <mathias.b.k@hotmail.com>
2019-02-13 14:15:51 +0100

    calculator simple

--- src/main/webapp/calcscript.js
+++ src/main/webapp/calcscript.js
@@ -3,5 +3,72 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function div(n1, n2) {
+    return n1 / n2;
+}
+function add(n1, n2) {
+    return n1 + n2;
+}
+var sub = function (n1, n2) {
+    return n1 - n2;
+}
+var mul = function (n1, n2) {
+    return n1 * n2;
+}
+document.getElementById("buttons").addEventListener("click", appendToDiv)
+function appendToDiv(e) {
+    e.preventDefault();
+    var text = document.getElementById("display").innerText;
+    text += e.target.innerText;
+    if(e.target.innerText.length == 1)
+    document.getElementById("display").innerText = text;
+}
 
+document.getElementById("calculate").addEventListener("click", equals)
 
+function equals(e) {
+    e.stopPropagation();
+    var text = "" + document.getElementById("display").innerText;
+    
+    var add1 = text.indexOf("+");
+    var sub1 = text.indexOf("-");
+    var mul1 = text.indexOf("*");
+    var div1 = text.indexOf("/");
+    
+    var n1 = "";
+    var n2 = "";
+    
+    var res;
+    if (add1 > -1){
+        numbers = text.split("+");
+        console.log(numbers)
+        n1 = parseInt(numbers[0]);
+        n2 = parseInt(numbers[1]);
+        res = add(n1,n2);
+        document.getElementById("display").innerText = ""+res;
+    }
+    if (sub1 > -1){
+        numbers = text.split("-");
+        console.log(numbers)
+        n1 = parseInt(numbers[0]);
+        n2 = parseInt(numbers[1]);
+        res = sub(n1,n2);
+        document.getElementById("display").innerText = ""+res;
+    }
+    if (mul1 > -1){
+        numbers = text.split("*");
+        console.log(numbers)
+        n1 = parseInt(numbers[0]);
+        n2 = parseInt(numbers[1]);
+        res = mul(n1,n2);
+        document.getElementById("display").innerText = ""+res;
+    }
+    if (div1 > -1){
+        numbers = text.split("/");
+        console.log(numbers)
+        n1 = parseInt(numbers[0]);
+        n2 = parseInt(numbers[1]);
+        res = div(n1,n2);
+        document.getElementById("display").innerText = ""+res;
+    }
+}
